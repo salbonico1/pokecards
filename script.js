a9a20a4cef382c624ba47f8eff2c5fb8dc4e10fa
Steve Albonico <salbonico@gmail.com>
2024-10-15 11:45:08 -0700

    added js to pack

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var pack = document.getElementById("pokemon-pack");
+    pack.addEventListener("click" , openPack);
+}
+
+function openPack(){
+    alert("card pack open");
+}
