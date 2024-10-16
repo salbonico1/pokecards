b6825350d351fb610529cfb24eec2af0f54df6ea
Steve Albonico <salbonico@gmail.com>
2024-10-15 22:47:14 -0700

    undid animation stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*
+
 window.onload = function() {
     var pack = document.getElementById("pokemon-pack");
     pack.addEventListener("click" , openPack);
@@ -38,45 +38,10 @@ function openPack(){
         cardDiv.appendChild(cardImg);
         document.getElementById("pokemon-cards-opened").appendChild(cardDiv);
     }
-} */
+} 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 const image = new Image();
 image.src = "./pokemon-cards/base set (10).jpg"; 
 
-image.onload = () => {
-    ctx.drawImage(image, 0, 0);
-
-    let tearPath = [];
-    let isDragging = false;
-
-    canvas.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        tearPath.push({ x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop });
-    });
-
-    canvas.addEventListener('mousemove', (e) => {
-        if (isDragging) {
-            tearPath.push({ x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop });
-            drawTear();
-        }
-    });
-
-    canvas.addEventListener('mouseup', () => {
-        isDragging = false;
-        tearPath = [];
-    });
-
-    function drawTear() {
-        ctx.drawImage(image, 0, 0);
-        ctx.beginPath();
-        ctx.moveTo(tearPath[0].x, tearPath[0].y);
-        for (let i = 1; i < tearPath.length; i++) {
-            ctx.lineTo(tearPath[i].x, tearPath[i].y);
-        }
-        ctx.closePath();
-        ctx.clip();
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-};
